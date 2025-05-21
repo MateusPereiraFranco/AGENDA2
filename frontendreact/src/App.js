9b0f21fc94bdbac629d69a876d679dc9bca52c43
Felipe <lookingthegames@gmail.com>
2025-05-21 18:55:37 -0300

    Relogio criado

--- frontendreact/src/App.js
+++ frontendreact/src/App.js
@@ -1,63 +1,62 @@
 // src/App.js
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './components/Login/Login';
-import Empresa from './components/Empresa/Empresa';
-import Usuario from './components/Usuario/Usuario';
-import Agenda from './components/Agenda/Agenda';
-import Horario from './components/Horario/Horario';
-import Navbar from './components/Navbar/Navbar';
-import NotFound from './components/NotFound/NotFound' // Importe o novo componente
-import { AuthProvider } from './context/AuthContext';
-import { Navigate } from 'react-router-dom';
-import ProtectedUsuarioRoute from './services/ProtectedUsuarioRoute';
-import ProtectedAgendaRoute from './services/ProtectedAgendaRoute';
-import ProtectedEmpresaRoute from './services/ProtectedEmpresaRoute';
-import ProtectedUpdatePasswordRoute from './services/ProtectedUpdatePasswordRoute';
-import UpdatePassword from './components/UpdatePassword/UpdatePassword';
-import ForgotPassword from './components/ForgotPassword/ForgotPassword';
-import VerifyToken from './components/VerifyToken/VerifyToken';
-import ResetPassword from './components/ResetPassword/ResetPassword';
-import ProtectedHorarioRoute from './services/ProtectedHorarioRoute copy';
-
+import React from "react";
+import { Navigate } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import Login from "./components/Login/Login";
+import Agenda from "./components/Agenda/Agenda";
+import Empresa from "./components/Empresa/Empresa";
+import Usuario from "./components/Usuario/Usuario";
+import Horario from "./components/Horario/Horario";
+import NotFound from "./components/NotFound/NotFound"; // Importe o novo componente
+import VerifyToken from "./components/VerifyToken/VerifyToken";
+import ProtectedAgendaRoute from "./services/ProtectedAgendaRoute";
+import ProtectedUsuarioRoute from "./services/ProtectedUsuarioRoute";
+import ProtectedEmpresaRoute from "./services/ProtectedEmpresaRoute";
+import ResetPassword from "./components/ResetPassword/ResetPassword";
+import UpdatePassword from "./components/UpdatePassword/UpdatePassword";
+import ForgotPassword from "./components/ForgotPassword/ForgotPassword";
+import ProtectedHorarioRoute from "./services/ProtectedHorarioRoute copy";
+import ProtectedUpdatePasswordRoute from "./services/ProtectedUpdatePasswordRoute";
 
 function App() {
-    return (
-        <AuthProvider>
-            <Router>
-                <Navbar />
-                <Routes>
-                    {/* Rota pública (não protegida) */}
-                    <Route path="/" element={<Navigate to="/login" />} />
-                    <Route path="/login" element={<Login />} />
+  return (
+    <AuthProvider>
+      <Router>
+        <Navbar />
+        <Routes>
+          {/* Rota pública (não protegida) */}
+          <Route path="/" element={<Navigate to="/login" />} />
+          <Route path="/login" element={<Login />} />
 
-                    <Route path="/forgot-password" element={<ForgotPassword />} />
-                    <Route path="/verify-token" element={<VerifyToken />} />
-                    <Route path="/reset-password" element={<ResetPassword />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/verify-token" element={<VerifyToken />} />
+          <Route path="/reset-password" element={<ResetPassword />} />
 
-                    {/* Rotas protegidas */}
-                    <Route element={<ProtectedEmpresaRoute />}>
-                        <Route path="/empresa" element={<Empresa />} />
-                    </Route>
-                    <Route element={<ProtectedUsuarioRoute />}>
-                        <Route path="/usuario/:id" element={<Usuario />} />
-                    </Route>
+          {/* Rotas protegidas */}
+          <Route element={<ProtectedEmpresaRoute />}>
+            <Route path="/empresa" element={<Empresa />} />
+          </Route>
+          <Route element={<ProtectedUsuarioRoute />}>
+            <Route path="/usuario/:id" element={<Usuario />} />
+          </Route>
 
-                    <Route element={<ProtectedAgendaRoute />}>
-                        <Route path="/agenda/:id" element={<Agenda />} />
-                    </Route>
-                    <Route element={<ProtectedHorarioRoute />}>
-                        <Route path="/horario/:id" element={<Horario />} />
-                    </Route>
-                    <Route element={<ProtectedUpdatePasswordRoute />}>
-                        <Route path="/atualizar-senha" element={<UpdatePassword />} />
-                    </Route>
-                    {/* Rota 404 - deve ser a última */}
-                    <Route path="*" element={<NotFound />} />
-                </Routes>
-            </Router>
-        </AuthProvider>
-    );
+          <Route element={<ProtectedAgendaRoute />}>
+            <Route path="/agenda/:id" element={<Agenda />} />
+          </Route>
+          <Route element={<ProtectedHorarioRoute />}>
+            <Route path="/horario/:id" element={<Horario />} />
+          </Route>
+          <Route element={<ProtectedUpdatePasswordRoute />}>
+            <Route path="/atualizar-senha" element={<UpdatePassword />} />
+          </Route>
+          {/* Rota 404 - deve ser a última */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
+  );
 }
 
 export default App;
--- frontendreact/src/components/Navbar/Navbar.js
+++ frontendreact/src/components/Navbar/Navbar.js
@@ -2,6 +2,9 @@
 import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom"; // Para redirecionar o usuário
 
+// Relógio
+import Relogio from "./relogio.js";
+
 //Icons
 import piratinha from "../../assets/piratinha.png";
 import HomeIcon from "@mui/icons-material/Home";
@@ -9,7 +12,7 @@ import InfoIcon from "@mui/icons-material/Info";
 import { HiOutlineBars3 } from "react-icons/hi2";
 import LogoutIcon from "@mui/icons-material/Logout"; // Ícone para o logout
 import PhoneRoundedIcon from "@mui/icons-material/PhoneRounded";
-import KeyIcon from '@mui/icons-material/Key';
+import KeyIcon from "@mui/icons-material/Key";
 import {
   Box,
   Drawer,
@@ -25,17 +28,20 @@ const getHomePath = (user) => {
   if (!user) return "/login";
   const { tipo_usuario, id_usuario, fk_empresa_id } = user;
 
-  return {
-    funcionario: `/agenda/${id_usuario}`,
-    secretario: `/usuario/${fk_empresa_id}`,
-    gerente: `/usuario/${fk_empresa_id}`,
-    admin: "/empresa",
-  }[tipo_usuario] || "/";
+  return (
+    {
+      funcionario: `/agenda/${id_usuario}`,
+      secretario: `/usuario/${fk_empresa_id}`,
+      gerente: `/usuario/${fk_empresa_id}`,
+      admin: "/empresa",
+    }[tipo_usuario] || "/"
+  );
 };
 
 const Navbar = () => {
   const [openMenu, setOpenMenu] = useState(false);
-  const { isAuthenticated, logout, setIsAuthenticated, user, setUser } = useAuth(); // Use o contexto de autenticação
+  const { isAuthenticated, logout, setIsAuthenticated, user, setUser } =
+    useAuth(); // Use o contexto de autenticação
   const navigate = useNavigate();
 
   const homePath = getHomePath(user);
@@ -49,7 +55,6 @@ const Navbar = () => {
     await logout();
     navigate("/login");
   };
-
 
   // Opções do menu
   const menuOptions = [
@@ -71,75 +76,78 @@ const Navbar = () => {
   ];
 
   return (
-    <nav>
-      <div className="nav-logo-container">
-        <img src={piratinha} alt="Logo" />
-      </div>
-      <div className="navbar-links-container">
-        {menuOptions.map((item) => (
-          <Link key={item.text} to={item.path || "#"} onClick={item.onClick}>
-            {item.text}
-          </Link>
-        ))}
-        {/* Botão de logout ao lado do Cart */}
-        {isAuthenticated && (
-          <>
-            <Link to="/atualizar-senha" style={{ cursor: "pointer" }}>
-              Trocar Senha
+      <nav>
+        <div className="nav-logo-container">
+          <Relogio />
+        </div>
+        <div className="navbar-links-container">
+          {menuOptions.map((item) => (
+            <Link key={item.text} to={item.path || "#"} onClick={item.onClick}>
+              {item.text}
             </Link>
+          ))}
+          {/* Botão de logout ao lado do Cart */}
+          {isAuthenticated && (
+            <>
+              <Link to="/atualizar-senha" style={{ cursor: "pointer" }}>
+                Trocar Senha
+              </Link>
 
-            <button className="logout-button" onClick={handleLogout}>
-              <LogoutIcon className="logout_icon" />
-              <span>Logout</span>
-            </button>
-
-          </>
-        )}
-      </div>
-      <div className="navbar-menu-container">
-        <HiOutlineBars3 onClick={() => setOpenMenu(true)} />
-      </div>
-      <Drawer open={openMenu} onClose={() => setOpenMenu(false)} anchor="right">
-        <Box
-          sx={{ width: 250 }}
-          role="presentation"
-          onClick={() => setOpenMenu(false)}
-          onKeyDown={() => setOpenMenu(false)}
+              <button className="logout-button" onClick={handleLogout}>
+                <LogoutIcon className="logout_icon" />
+                <span>Logout</span>
+              </button>
+            </>
+          )}
+        </div>
+        <div className="navbar-menu-container">
+          <HiOutlineBars3 onClick={() => setOpenMenu(true)} />
+        </div>
+        <Drawer
+          open={openMenu}
+          onClose={() => setOpenMenu(false)}
+          anchor="right"
         >
-          <List>
-            {menuOptions.map((item) => (
-              <ListItem key={item.text} disablePadding>
-                <ListItemButton onClick={item.onClick}>
-                  <ListItemIcon>{item.icon}</ListItemIcon>
-                  <ListItemText primary={item.text} />
-                </ListItemButton>
-              </ListItem>
-            ))}
-            {/* Adiciona o botão de logout no menu lateral */}
-            {isAuthenticated && (
-              <>
-                <ListItem disablePadding>
-                  <ListItemButton onClick={handleNavigateToChangePassword}>
-                    <ListItemIcon>
-                      <KeyIcon /> {/* Ou outro ícone como KeyRoundedIcon */}
-                    </ListItemIcon>
-                    <ListItemText primary="Trocar Senha" />
+          <Box
+            sx={{ width: 250 }}
+            role="presentation"
+            onClick={() => setOpenMenu(false)}
+            onKeyDown={() => setOpenMenu(false)}
+          >
+            <List>
+              {menuOptions.map((item) => (
+                <ListItem key={item.text} disablePadding>
+                  <ListItemButton onClick={item.onClick}>
+                    <ListItemIcon>{item.icon}</ListItemIcon>
+                    <ListItemText primary={item.text} />
                   </ListItemButton>
                 </ListItem>
-                <ListItem disablePadding>
-                  <ListItemButton onClick={handleLogout}>
-                    <ListItemIcon>
-                      <LogoutIcon />
-                    </ListItemIcon>
-                    <ListItemText primary="Logout" />
-                  </ListItemButton>
-                </ListItem>
-              </>
-            )}
-          </List>
-        </Box>
-      </Drawer>
-    </nav>
+              ))}
+              {/* Adiciona o botão de logout no menu lateral */}
+              {isAuthenticated && (
+                <>
+                  <ListItem disablePadding>
+                    <ListItemButton onClick={handleNavigateToChangePassword}>
+                      <ListItemIcon>
+                        <KeyIcon /> {/* Ou outro ícone como KeyRoundedIcon */}
+                      </ListItemIcon>
+                      <ListItemText primary="Trocar Senha" />
+                    </ListItemButton>
+                  </ListItem>
+                  <ListItem disablePadding>
+                    <ListItemButton onClick={handleLogout}>
+                      <ListItemIcon>
+                        <LogoutIcon />
+                      </ListItemIcon>
+                      <ListItemText primary="Logout" />
+                    </ListItemButton>
+                  </ListItem>
+                </>
+              )}
+            </List>
+          </Box>
+        </Drawer>
+      </nav>
   );
 };
 
